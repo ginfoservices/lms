bce729972f93d4e7c3bfffb3cc98f4027c9f829c
ginfoservices <rabi.stha@live.com>
2019-02-17 00:05:04 +0545

    minor changes

--- index.php
+++ index.php
@@ -12,29 +12,24 @@ include('admin/dbcon.php');
                         <input type="text" class="form-control" id="susername" name="susername" placeholder="Username" required>
                     </div>
                     <div class="form-group">
-
                         <input type="password" class="form-control" id="spassword" name="spassword" placeholder="Password" required>
                     </div>
                     <button id="signin" name="login" class="btn btn-info" type="submit">Sign in</button>
-
                 </form>
             </div>
         </div>
         <div class="row">
             <div class="col-md-6">
-
                 <form id="signin_student" class="form-signin" method="post">
                     <h3 class="form-signin-heading"><i class="icon-lock"></i> Sign up as Student</h3>
-
                     <div class="form-group">
                         <label>Class</label>
                         <select name="class_id" class="form-control span5">
                             <?php $query = mysqli_query($conn, "select * from class order by class_name ") or die(mysqli_error()); while ($row = mysqli_fetch_array($query)) { ?>
-                            <option value="<?php echo $row['class_id']; ?>"><?php echo $row['class_name']; ?> </option>
+                                <option value="<?php echo $row['class_id']; ?>"><?php echo $row['class_name']; ?> </option>
                             <?php } ?>
                         </select>
                     </div>
-
                     <div class="form-group">
                         <input type="text" class="form-control" id="firstname" name="firstname" placeholder="Firstname" required>
                     </div>
@@ -48,24 +43,19 @@ include('admin/dbcon.php');
                         <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
                     </div>
                     <div class="form-group">
-                        <input type="password" class="form-control" id="cpassword" name="cpassword"
-                            placeholder="Re-type Password" required>
+                        <input type="password" class="form-control" id="cpassword" name="cpassword" placeholder="Re-type Password" required>
                     </div>
                     <button id="ssignin" name="ssignup" class="btn btn-info btn-block" type="submit">Sign Up</button>
                 </form>
-
             </div>
             <div class="col-md-6">
-
                 <form id="signin_teacher" class="form-signin" method="post">
                     <h3 class="form-signin-heading"><i class="icon-lock"></i> Sign up as Teacher</h3>
-
                     <div class="form-group">
                         <label>Department</label>
                         <select name="department_id" class="form-control span12">
                             <?php $query = mysqli_query($conn, "select * from department order by department_name ") or die(mysql_error()); while ($row = mysqli_fetch_array($query)) { ?>
-                            <option value="<?php echo $row['department_id'] ?>"><?php echo $row['department_name']; ?>
-                            </option>
+                                <option value="<?php echo $row['department_id'] ?>"><?php echo $row['department_name']; ?></option>
                             <?php } ?>
                         </select>
                     </div>
@@ -75,7 +65,6 @@ include('admin/dbcon.php');
                     <div class="form-group">
                         <input type="text" class="form-control" name="tlastname" placeholder="Lastname" required>
                     </div>
-
                     <div class="form-group">
                         <input type="text" class="form-control" id="tusername" name="tusername" placeholder="Username" required>
                     </div>
@@ -86,21 +75,18 @@ include('admin/dbcon.php');
                         <input type="password" class="form-control" id="tcpassword" name="tcpassword" placeholder="Re-type Password" required>
                     </div>
                     <div class="form-group">
-                        <button id="tsignin" name="tsignup" class="btn btn-info btn-block" type="submit">Sign
-                            Up</button>
+                        <button id="tsignin" name="tsignup" class="btn btn-info btn-block" type="submit">Sign Up</button>
                 </form>
-
             </div>
         </div>
         <div class="row">
             <div class="col-md-12">
-
                 <ul class="nav justify-content-center">
                     <li class="nav-item">
                         <a class="nav-link" href="index.php">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="about.php">About</a>
+                        <a class="nav-link" href="#about" data-toggle="modal">About</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="calendar_of_events.php">Calendar of Events</a>
@@ -116,16 +102,10 @@ include('admin/dbcon.php');
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#history" data-toggle="modal">History</a>
-                    </li>
+                    </li>                 
                 </ul>
             </div>
-
-
-
         </div>
-
-
-
     </div>
     <!--/.nav-collapse -->
     <script>
@@ -294,7 +274,6 @@ include('admin/dbcon.php');
                                     </p>
                                 </div>
                             </div>
-
                         </div>
                         <div class="col-md-6">
                             <div class="card">
@@ -329,9 +308,9 @@ include('admin/dbcon.php');
                 </div>
                 <div class="modal-body">
                     <?php
-                    $mission_query = mysqli_query($conn, "select * from content where title  = 'Directories' ") or die(mysql_error());
-                    $mission_row = mysqli_fetch_array($mission_query);
-                    echo $mission_row['content'];
+                        $mission_query = mysqli_query($conn, "select * from content where title  = 'Directories' ") or die(mysql_error());
+                        $mission_row = mysqli_fetch_array($mission_query);
+                        echo $mission_row['content'];
                     ?>
                 </div>
                 <div class="modal-footer">
@@ -340,8 +319,35 @@ include('admin/dbcon.php');
             </div>
         </div>
     </div>
-
-
+    <!-- Modal -->
+    <div class="modal fade" id="about" tabindex="-1" role="dialog" aria-labelledby="modelTitleId3" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">About</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <?php
+                        $mission_query = mysqli_query($conn,"select * from content where title  = 'mission' ")or die(mysqli_error($mysqli));
+                        $mission_row = mysqli_fetch_array($mission_query);
+                        echo $mission_row['content'];
+                    ?>
+                    <hr />
+                    <?php
+                        $mission_query = mysqli_query($conn,"select * from content where title  = 'vision' ")or die(mysqli_error($mysqli));
+                        $mission_row = mysqli_fetch_array($mission_query);
+                        echo $mission_row['content'];
+                    ?>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <!-- Modal -->
     <div class="modal fade" id="campuses" tabindex="-1" role="dialog" aria-labelledby="modelTitleId4" aria-hidden="true">
         <div class="modal-dialog" role="document">
@@ -354,9 +360,9 @@ include('admin/dbcon.php');
                 </div>
                 <div class="modal-body">
                     <?php
-                    $mission_query = mysqli_query($conn, "select * from content where title  = 'Campuses' ") or die(mysql_error());
-                    $mission_row = mysqli_fetch_array($mission_query);
-                    echo $mission_row['content'];
+                        $mission_query = mysqli_query($conn, "select * from content where title  = 'Campuses' ") or die(mysql_error());
+                        $mission_row = mysqli_fetch_array($mission_query);
+                        echo $mission_row['content'];
                     ?>
                 </div>
                 <div class="modal-footer">
@@ -365,7 +371,6 @@ include('admin/dbcon.php');
             </div>
         </div>
     </div>
-
     <!-- Modal -->
     <div class="modal fade" id="history" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
         <div class="modal-dialog" role="document">
@@ -381,7 +386,7 @@ include('admin/dbcon.php');
                         $mission_query = mysqli_query($conn, "select * from content where title  = 'History' ") or die(mysql_error());
                         $mission_row = mysqli_fetch_array($mission_query);
                         echo $mission_row['content'];
-                        ?>
+                    ?>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -391,5 +396,4 @@ include('admin/dbcon.php');
     </div>
     <?php include('admin/includes/footer.php'); ?>
 </body>
-
 </html>
